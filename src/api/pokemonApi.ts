de683c988a1e0a42ffd3bdf5c87586a3a32bf4e6
Pooja Khot <pkhot@uplandsoftware.com>
2026-03-20 17:05:48 +0530

    feat: added pokemon detail api

--- src/api/pokemonApi.ts
+++ src/api/pokemonApi.ts
@@ -1,5 +1,11 @@
 export const fetchPokemonList = async () => {
   const res = await fetch("https://pokeapi.co/api/v2/pokemon");
-  if (!res.ok) throw new Error("Failed to fetch");
+  if (!res.ok) throw new Error("Failed to fetch pokemon");
+  return res.json();
+};
+
+export const fetchPokemonDetail = async (name: string) => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  if (!res.ok) throw new Error("Failed to fetch pokemon detail");
   return res.json();
 };
